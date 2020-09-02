bfb2f8b0776d740fd7115d8a3ee8aac5f9f26370
marijndegen <marijndegen96@hotmail.com>
2020-09-02 12:12:53 +0200

    quicky

--- flutter_app/auto_flutter_app/lib/middleware/app_middleware.dart
+++ flutter_app/auto_flutter_app/lib/middleware/app_middleware.dart
@@ -3,8 +3,8 @@ import 'package:auto_flutter_app/middleware/status_middleware.dart';
 import 'package:redux/redux.dart';
 import 'package:auto_flutter_app/state/app_state.dart';
 
-// final String api = "http://192.168.4.1:80/";
-final String api = "http://192.168.1.6:80/";
+final String api = "http://192.168.4.1:80/";
+// final String api = "http://192.168.1.6:80/";
 
 
 List<Middleware<AppState>> createAppMiddleware(client) {
